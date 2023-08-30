1172a3a64b2e5aea1ca9227439ca0b04ddfd6db4
Aulrata <Aulrata@gamil.com>
2023-08-30 19:43:47 +0300

    Login window added

---
+++ Controllers/AuthorizationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessCenter.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Views/Authorization/Login.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FitnessCenter.Views.Authorization
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+            Console.WriteLine();
+        }
+    }
+}
